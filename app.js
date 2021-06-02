85cfc7f906e5848719206348460f9a9359297eea
EzeBauer <83975962+EzeBauer@users.noreply.github.com>
2021-06-02 16:31:13 -0300

    Trabajo Playground Mercado liebre

---
+++ app.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+const port= 3080
+const path= require ('path')
+
+app.listen(port, () => console.log('Servidor corriendo en puerto '+ port))
+
+app.get('/', (req, res) => res.sendFile(path.resolve(__dirname, "./views/home.html")))
+
+app.use(express.static('public'));

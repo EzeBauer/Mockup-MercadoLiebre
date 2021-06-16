a72fe15dffbf04695f918dc08fadfae476715fe1
EzeBauer <83975962+EzeBauer@users.noreply.github.com>
2021-06-16 06:48:27 -0300

    Aplicando estilos a MercadoLiebre

--- app.js
+++ app.js
@@ -3,7 +3,9 @@ const app = express();
 const port= 3080
 const path= require ('path')
 
-app.listen(port, () => console.log('Servidor corriendo en puerto '+ port))
+app.listen(port, () =>
+  console.log("Servidor corriendo en puerto http://localhost:" + port)
+);
 
 app.get('/', (req, res) => res.sendFile(path.resolve(__dirname, "./views/home.html")))
 
